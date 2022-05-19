bf6085299caaf403911fbe63fce1cc20c081a82a
KaiMatsui <kai0927@fuji.waseda.jp>
2022-05-19 17:58:26 +0900

    add extentioin

--- movie/movie_app.py
+++ movie/movie_app.py
@@ -6,12 +6,13 @@ class Movie():
     def __init__(self):
         pass
 
-    def frame(self, file, directry, freq=1, magnification=1):
+    def frame(self, file, directry, freq=1, magnification=1, ext='.png'):
         self.movie_app = MovieApp()
         self.movie_app.set_filename(file)
         self.movie_app.set_directry_name(directry)
         self.movie_app.set_freq(freq)
         self.movie_app.set_magnification(magnification)
+        self.movie_app.set_extension(ext)
         self.movie_app.make_directry()
         self.movie_app.capture_video()
         self.movie_app.frame_loop()
@@ -30,6 +31,9 @@ class MovieApp():
     def set_magnification(self, magnification):
         self.magnification = float(magnification)
 
+    def set_extension(self, ext):
+        self.ext = ext
+
     def make_directry(self):
         os.makedirs(self.directry, exist_ok=True)
 
@@ -43,14 +47,18 @@ class MovieApp():
             if is_captured:
                 if frame_number % self.freq == 0:
                     new_frame = self.resize_frame(c_frame)
-                    cv2.imwrite(self.directry + '/' + str((1000 + frame_number) * 10000000) + '.png', new_frame)
+                    cv2.imwrite(self.directry + '/' + str((100000 + frame_number) * 100000) + self.ext, new_frame)
             else:
                 break
             frame_number += 1
             if frame_number % 100 == 0:
                 print(frame_number)
+                self.print_size(new_frame)
 
     def resize_frame(self, frame):
         height = frame.shape[0]
         width = frame.shape[1]
         return cv2.resize(frame, (int(width * self.magnification), int(height * self.magnification)))
+
+    def print_size(self, frame):
+        print('width:{}, height: {}'.format(frame.shape[1], frame.shape[0]))
